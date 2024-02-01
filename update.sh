422c596c7a2ed63b529aa3596578609817973c41
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-02-01 15:46:23 +0100

    feat: add update function

---
+++ update.sh
@@ -0,0 +1,3 @@
+curl -L https://github.com/helsingborg-stad/wordpress-migration-scripts/raw/main/db-export.sh > db-export.sh
+curl -L https://github.com/helsingborg-stad/wordpress-migration-scripts/raw/main/db-import.sh > db-import.sh
+curl -L https://github.com/helsingborg-stad/wordpress-migration-scripts/raw/main/update.sh > update.sh
