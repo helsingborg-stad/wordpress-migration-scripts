895af3d7659a589951d89f925e501c613544dc5d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-02-02 12:51:21 +0100

    fix: make string replacable.

--- db-import.sh
+++ db-import.sh
@@ -60,7 +60,7 @@ import_site_database() {
         exit 1
     fi
 
-    SEARCH_REPLACE_CMD="wp search-replace '$FROM_DOMAIN' '$TO_DOMAIN' ${PREFIX}_* ${BASE_PREFIX}blogs --network --skip-columns=guid --all-tables"
+    SEARCH_REPLACE_CMD="wp search-replace $FROM_DOMAIN $TO_DOMAIN ${PREFIX}_* ${BASE_PREFIX}blogs --network --skip-columns=guid --all-tables"
     color_echo yellow "  Performing domain replacement: $SEARCH_REPLACE_CMD"
     $SEARCH_REPLACE_CMD
 
