59a58987779e500da406aa800b267f23ec09868a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-02-06 14:01:36 +0100

    Incorporate a cachebust in the download script

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
-curl -L https://github.com/helsingborg-stad/wordpress-migration-scripts/raw/main/db-export.sh > db-export.sh
-curl -L https://github.com/helsingborg-stad/wordpress-migration-scripts/raw/main/db-import.sh > db-import.sh
-curl -L https://github.com/helsingborg-stad/wordpress-migration-scripts/raw/main/update.sh > update.sh
+# Generate a unique random cachebust
+cache_bust=$(uuidgen)
+
+# Use the random cachebust in the URLs
+curl -L "https://raw.githubusercontent.com/helsingborg-stad/wordpress-migration-scripts/main/db-export.sh?$cache_bust" > db-export.sh
+curl -L "https://raw.githubusercontent.com/helsingborg-stad/wordpress-migration-scripts/main/db-import.sh?$cache_bust" > db-import.sh
+curl -L "https://raw.githubusercontent.com/helsingborg-stad/wordpress-migration-scripts/main/update.sh?$cache_bust" > update.sh
